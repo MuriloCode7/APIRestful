3f7bb3e30d5f55ff441e406cc5e8738f2b0726ac
MuriloMagal <murilomagalhaes60@gmail.com>
2023-09-23 01:05:17 -0300

    Feat: Create Show Order Service

---
+++ src/modules/orders/services/ShowOrderService.ts
@@ -0,0 +1,26 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { OrdersRepository } from '../typeorm/repositories/OrdersRepository';
+import Order from '../typeorm/entities/Order';
+
+interface IRequest {
+  id: string;
+}
+
+class ShowOrderService {
+  public async execute({ id }: IRequest): Promise<Order> {
+    const ordersRepository = getCustomRepository(OrdersRepository);
+
+    const order = await ordersRepository.findById(id);
+
+    /*
+    Verifica se o cliente existe
+    */
+    if (!order) {
+      throw new AppError('Order not found.');
+    }
+    return order;
+  }
+}
+
+export default ShowOrderService;
