a1aa091138447b2804abc3134cc795f4707f8dc3
MuriloMagal <murilomagalhaes60@gmail.com>
2023-09-23 01:01:51 -0300

    Feat: Conclusion Create Order Service

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -3,9 +3,10 @@ import { getCustomRepository } from 'typeorm';
 import Order from '../typeorm/entities/Order';
 import { CustomerRepository } from '@modules/customers/typeorm/repositories/CustomersRepository';
 import { ProductRepository } from '@modules/products/typeorm/repositories/ProductsRepository';
+import { OrdersRepository } from '../typeorm/repositories/OrdersRepository';
 
 interface IProduct {
-  product_id: string;
+  id: string;
   quantity: number;
 }
 
@@ -22,9 +23,78 @@ class CreateOrderService {
 
     const customerExists = await customersRepository.findById(customer_id);
 
+    /*
+    Verifica se o cliente existe
+    */
     if (!customerExists) {
       throw new AppError('Could not find any customer with the given id.');
     }
+
+    const existsProducts = await productsRepository.findAllByIds(products);
+
+    /*
+    Verifica se foi encontrado algum produto a partir dos ids informados
+    */
+    if (!existsProducts.length) {
+      throw new AppError('Could not find any products with the given ids.');
+    }
+
+    // Guarda todos os ids de produtos que foram encontrados no pedido
+    const existsProductsIds = existsProducts.map(product => product.id);
+
+    /* verifica se a quantidade de produtos encontrados eh a mesma quantidade de
+    produtos no pedido.
+    Se não for, esta errado
+    */
+    const checkInexistentProducts = products.filter(
+      product => !existsProductsIds.includes(product.id),
+    );
+
+    if (checkInexistentProducts.length) {
+      throw new AppError(
+        `Could not find product ${checkInexistentProducts[0].id}`,
+      );
+    }
+
+    /* Verifica se ha a quantidade que esta sendo vendida no estoque */
+    const quantityAvailable = products.filter(
+      product =>
+        existsProducts.filter(p => p.id === product.id)[0].quantity <
+        product.quantity,
+    );
+
+    if (quantityAvailable.length) {
+      throw new AppError(
+        `The quantity ${quantityAvailable[0].quantity}
+        is not available for ${quantityAvailable[0].id}`,
+      );
+    }
+
+    const serializedProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: existsProducts.filter(p => p.id === product.id)[0].price,
+    }));
+
+    const order = await ordersRepository.createOrder({
+      customer: customerExists,
+      products: serializedProducts,
+    });
+
+    const { order_products } = order;
+
+    // Decrementa a quantidade do produto no estoque
+    const updatedProductQuantity = order_products.map(product => ({
+      id: product.product_id,
+      quantity:
+        existsProducts.filter(p => p.id === product.id)[0].quantity -
+        product.quantity,
+    }));
+
+    // Salva o produto com a quantidade alterada
+    await productsRepository.save(updatedProductQuantity);
+
+    return order;
   }
 }
 
--- src/modules/orders/typeorm/entities/OrdersProducts.ts
+++ src/modules/orders/typeorm/entities/OrdersProducts.ts
@@ -20,12 +20,6 @@ class OrdersProducts {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column('decimal')
-  price: number;
-
-  @Column('int')
-  quantity: number;
-
   @ManyToOne(() => Order, order => order.order_products)
   @JoinColumn({ name: 'order_id' })
   order: Order;
@@ -33,6 +27,18 @@ class OrdersProducts {
   @ManyToOne(() => Product, product => product.order_products)
   @JoinColumn({ name: 'product_id' })
   product: Product;
+
+  @Column()
+  order_id: string;
+
+  @Column()
+  product_id: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
 
   @CreateDateColumn()
   created_at: Date;
