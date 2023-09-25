3c11f1df8233eff899878a01601bb7516b7732e9
MuriloMagal <murilomagalhaes60@gmail.com>
2023-09-25 10:22:32 -0300

    Feat: Implements Redis Cache in List Prods. Servi

--- src/modules/products/services/ListProductsService.ts
+++ src/modules/products/services/ListProductsService.ts
@@ -2,12 +2,17 @@ import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+import RedisCache from '@shared/cache/RedisCache';
 
 class ListProductsService {
   public async execute(): Promise<Product[]> {
     const productsRepository = getCustomRepository(ProductRepository);
 
+    const redisCache = new RedisCache();
+
     const products = await productsRepository.find();
+
+    await redisCache.save('teste', 'teste');
 
     // Se nenhum produto for encontrado, aparece uma mensagem
     if ((await products).length == 0) {
--- src/shared/cache/RedisCache.ts
+++ src/shared/cache/RedisCache.ts
@@ -9,7 +9,7 @@ export default class RedisCache {
   }
 
   public async save(key: string, value: any): Promise<void> {
-    console.log(key, value);
+    await this.client.set(key, JSON.stringify(value));
   }
 
   /*
