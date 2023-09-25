a88f2c7fc6c3e7c8ac80f2a06a8828aac9bc4c99
MuriloMagal <murilomagalhaes60@gmail.com>
2023-09-25 01:05:12 -0300

    Feat: Class Redis Cache

---
+++ src/shared/cache/RedisCache.ts
@@ -0,0 +1,22 @@
+import Redis, { Redis as RedisClient } from 'ioredis';
+import cacheConfig from '@config/cache';
+
+export default class RedisCache {
+  private client: RedisClient;
+
+  constructor() {
+    this.client = new Redis(cacheConfig.config.redis);
+  }
+
+  public async save(key: string, value: any): Promise<void> {
+    console.log(key, value);
+  }
+
+  /*
+  Como nao é possivel saber o tipo de retorno que a funcao vai trazer, usa-se um retorno generico
+  como o <T>
+   */
+  //public async recover<T>(key: string): Promise<T | null> {}
+
+  //public async invalidate(key: string): Promise<void> {}
+}
