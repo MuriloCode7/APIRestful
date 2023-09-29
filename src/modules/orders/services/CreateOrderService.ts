6f61bbf436a4ef7e532923e56b5031ed6bdf4718
MuriloMagal <murilomagalhaes60@gmail.com>
2023-09-29 13:26:33 -0300

    Feat: Create Infra Folders

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -1,9 +1,9 @@
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
-import Order from '../typeorm/entities/Order';
-import { CustomerRepository } from '@modules/customers/typeorm/repositories/CustomersRepository';
-import { ProductRepository } from '@modules/products/typeorm/repositories/ProductsRepository';
-import { OrdersRepository } from '../typeorm/repositories/OrdersRepository';
+import Order from '../infra/typeorm/entities/Order';
+import { CustomerRepository } from '@modules/customers/infra/typeorm/repositories/CustomersRepository';
+import { ProductRepository } from '@modules/products/infra/typeorm/repositories/ProductsRepository';
+import { OrdersRepository } from '../infra/typeorm/repositories/OrdersRepository';
 
 interface IProduct {
   id: string;
