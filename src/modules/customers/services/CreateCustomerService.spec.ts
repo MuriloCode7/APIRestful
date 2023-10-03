034bc3e74d201d894f295c2d468dd84796cea84f
MuriloMagal <murilomagalhaes60@gmail.com>
2023-10-03 13:07:43 -0300

    Feat: Conclusion Create Customer Test

--- src/modules/customers/services/CreateCustomerService.spec.ts
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -1,24 +1,29 @@
 import 'reflect-metadata';
-import CreateCustomerService from "./CreateCustomerService";
-import { FakeCustomersRepository } from "../domain/repositories/fakes/FakeCustomersRepository";
+import CreateCustomerService from './CreateCustomerService';
+import { FakeCustomersRepository } from '../domain/repositories/fakes/FakeCustomersRepository';
+import AppError from '@shared/errors/AppError';
 
-describe('CreateCustomer',  () => {
+const fakeCustomersRepository = new FakeCustomersRepository();
+const createCustomer = new CreateCustomerService(fakeCustomersRepository);
+
+describe('CreateCustomer', () => {
   it('Should be able to create a new customer', async () => {
-    const fakeCustomersRepository = new FakeCustomersRepository();
-    const createCustomer = new CreateCustomerService(fakeCustomersRepository);
-
     const customer = await createCustomer.execute({
       name: 'Cliente teste',
-      email: 'teste@teste.com'
+      email: 'teste@teste.com',
     });
 
     /* Para validar se o clliente foi criado corretamente, verificamos se ele possui uma propriedade
     que é gerada automaticamente na criação dele */
     expect(customer).toHaveProperty('id');
-
-  })
+  });
 
   it('Should not be able to create two customers with the same email', () => {
-    expect(1).toBe(1);
-  })
+    expect(
+      createCustomer.execute({
+        name: 'Cliente teste',
+        email: 'teste@teste.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
